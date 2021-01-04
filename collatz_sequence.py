a0781557d4f6adc78008334aa5ffa8eca46cf0e3
john-w-c <33558191+john-w-c@users.noreply.github.com>
2021-01-03 18:13:45 -0800

    feat: Create user prompt and condition check upon reaching 1

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -1,10 +1,11 @@
 def collatz(number):
-    if number % 2 == 0:
-        result = number //2
-        print(result)
-    else:
+    if number % 2 == 0: # if even
+        result = number // 2
+    else: # if odd
         result = 3 * number + 1
-        print(result)
+    print(result)
     return result
 
-    
+current_number = int(input('Please input an integer:\n'))
+while current_number != 1:
+    current_number = collatz(current_number)
